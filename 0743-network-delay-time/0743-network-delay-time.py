b9f89677e20a068d0f02b15fd336442f15140fcd
Shivang Agarwal <itshivang@gmail.com>
2026-07-01 17:55:57 +0530

    Time: 381 ms (34.3%), Space: 14.4 MB (99.06%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -6,7 +6,7 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        from collections import deque
+        import heapq
         dis = [float("inf")]*(n+1)
         dis[k] = 0
         dis[0] = 0
@@ -15,14 +15,14 @@ class Solution(object):
             adj.append([])
         for src,dst,time in times:
             adj[src].append([dst,time])
-        q = deque()
-        q.append([k,0])
+        q = []
+        heapq.heappush(q,[k,0])
         while q:
-            dst,time = q.popleft()
+            dst,time = heapq.heappop(q)
             for i,j in adj[dst]:
                 if time+j<dis[i]:
                     dis[i] = time+j
-                    q.append([i,time+j])
+                    heapq.heappush(q,[i,time+j])
         maxi = max(dis)
         if maxi == float("inf"):
             return -1
