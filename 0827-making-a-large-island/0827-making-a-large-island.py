afd513a3782f0664e76c2f15e3803919293c8284
Shivang Agarwal <itshivang@gmail.com>
2026-07-09 20:11:53 +0530

    Time: 970 ms (87.04%), Space: 43.8 MB (17.59%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -5,8 +5,9 @@ class Solution(object):
         :rtype: int
         """
         n = len(grid)
-        parent = list(range(n*n))
+        parent = list(range(n * n))
         size = []
+
         for i in range(n):
             for j in range(n):
                 size.append(grid[i][j])
@@ -21,44 +22,51 @@ class Solution(object):
                 if not grid[i][j]:
                     continue
 
-                u = n*i + j
+                u = n * i + j
 
-                if i < n-1 and grid[i+1][j] and find(u) != find(u+n):
-                    if size[find(u)] <= size[find(u+n)]:
-                        size[find(u+n)] += size[find(u)]
-                        parent[find(u)] = find(u+n)
-                    else:
-                        size[find(u)] += size[find(u+n)]
-                        parent[find(u+n)] = find(u)
+                if i < n - 1 and grid[i + 1][j]:
+                    ru = find(u)
+                    rv = find(u + n)
+                    if ru != rv:
+                        if size[ru] <= size[rv]:
+                            size[rv] += size[ru]
+                            parent[ru] = rv
+                        else:
+                            size[ru] += size[rv]
+                            parent[rv] = ru
 
-                if j < n-1 and grid[i][j+1] and find(u) != find(u+1):
-                    if size[find(u)] <= size[find(u+1)]:
-                        size[find(u+1)] += size[find(u)]
-                        parent[find(u)] = find(u+1)
-                    else:
-                        size[find(u)] += size[find(u+1)]
-                        parent[find(u+1)] = find(u)
+                if j < n - 1 and grid[i][j + 1]:
+                    ru = find(u)
+                    rv = find(u + 1)
+                    if ru != rv:
+                        if size[ru] <= size[rv]:
+                            size[rv] += size[ru]
+                            parent[ru] = rv
+                        else:
+                            size[ru] += size[rv]
+                            parent[rv] = ru
 
         maxi = max(size)
 
         for i in range(n):
             for j in range(n):
                 if not grid[i][j]:
-                    u = n*i + j
+                    u = n * i + j
                     unique = set()
 
-                    if i > 0 and grid[i-1][j]:
-                        unique.add(find(u-n))
-                    if j > 0 and grid[i][j-1]:
-                        unique.add(find(u-1))
-                    if i < n-1 and grid[i+1][j]:
-                        unique.add(find(u+n))
-                    if j < n-1 and grid[i][j+1]:
-                        unique.add(find(u+1))
+                    if i > 0 and grid[i - 1][j]:
+                        unique.add(find(u - n))
+                    if j > 0 and grid[i][j - 1]:
+                        unique.add(find(u - 1))
+                    if i < n - 1 and grid[i + 1][j]:
+                        unique.add(find(u + n))
+                    if j < n - 1 and grid[i][j + 1]:
+                        unique.add(find(u + 1))
 
                     area = 1
                     for k in unique:
                         area += size[k]
+
                     maxi = max(maxi, area)
 
         return maxi
