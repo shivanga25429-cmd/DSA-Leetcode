6765f588582fbe6d3a3238e470eac8970caf7a67
Shivang Agarwal <itshivang@gmail.com>
2026-07-15 17:33:02 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.74%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,13 +5,12 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        dp = [0]*n
         if n==1:
             return nums[0]
-        dp[0] = nums[0]
-        dp[1] = nums[1]
-        maxi = dp[0]
+        maxi = nums[0]
+        prev = nums[1]
         for i in range(2,n):
-            dp[i] = maxi + nums[i]
-            maxi = max(maxi,dp[i-1])
-        return max(dp[-1],maxi)
+            curr = maxi + nums[i]
+            maxi = max(maxi,prev)
+            prev = curr
+        return max(prev,maxi)
