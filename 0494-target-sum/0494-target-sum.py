22f19981c35eb8543c594a9aa74772193076e993
Shivang Agarwal <itshivang@gmail.com>
2026-08-14 14:52:44 +0530

    Time: 114 ms (49.32%), Space: 12.2 MB (98.52%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -10,17 +10,17 @@ class Solution(object):
         if tsum-abs(target)<0 or (target+tsum)%2 != 0:
             return 0
         prev = [0]*(tsum+1)
-        curr = [0]*(tsum+1)
         prev[0] += 1
         prev[nums[0]] += 1
         rtarget = (target+tsum)//2
         for i in range(1,len(nums)):
+            curr = [0]*(tsum+1)
             for j in range(tsum+1):
                 curr[j] += prev[j]
                 if nums[i]<=j:
                     curr[j] += prev[j-nums[i]]
-            prev = curr[::]
-            curr = [0]*(tsum+1)
+            prev = curr
+            
         return prev[rtarget]
 
         
