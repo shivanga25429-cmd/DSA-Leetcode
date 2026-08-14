8f53a1f40d5178df80e7de8e7b88fdf013e78313
Shivang Agarwal <itshivang@gmail.com>
2026-08-14 14:50:21 +0530

    Time: 113 ms (49.54%), Space: 12.3 MB (98.52%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -9,19 +9,22 @@ class Solution(object):
         tsum = sum(nums)
         if tsum-abs(target)<0 or (target+tsum)%2 != 0:
             return 0
-        dp = []
-        for _ in nums:
-            arr = [0]*(tsum+1)
-            dp.append(arr)
-        dp[0][0] += 1
-        dp[0][nums[0]] += 1
+        prev = [0]*(tsum+1)
+        curr = [0]*(tsum+1)
+        prev[0] += 1
+        prev[nums[0]] += 1
         rtarget = (target+tsum)//2
         for i in range(1,len(nums)):
             for j in range(tsum+1):
-                dp[i][j] += dp[i-1][j]
+                curr[j] += prev[j]
                 if nums[i]<=j:
-                    dp[i][j] += dp[i-1][j-nums[i]]
-        return dp[-1][rtarget]
+                    curr[j] += prev[j-nums[i]]
+            prev = curr[::]
+            curr = [0]*(tsum+1)
+        return prev[rtarget]
+
+        
+
 
 
         
