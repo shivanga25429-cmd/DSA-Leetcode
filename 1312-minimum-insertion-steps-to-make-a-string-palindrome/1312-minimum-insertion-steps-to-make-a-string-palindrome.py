cd21b152f2f9bbd56075e3ee4c592ee42c96cca5
Shivang Agarwal <itshivang@gmail.com>
2026-09-06 14:21:45 +0530

    Time: 687 ms (66.51%), Space: 12.4 MB (89.59%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minInsertions(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        prev = [0]*(n+1)
+        for i in range(1,n+1):
+            curr = [0]*(n+1)
+            for j in range(1,n+1):
+                if s[i-1] == s[n-j]:
+                    curr[j] = 1 + prev[j-1]
+                else:
+                    curr[j] = max(prev[j], curr[j-1])
+            prev = curr
+        return n - prev[-1]
+        
