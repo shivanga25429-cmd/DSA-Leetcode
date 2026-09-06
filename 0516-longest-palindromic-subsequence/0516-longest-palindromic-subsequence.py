bda6257eab1d7413245ad8a110ce917893c89537
Shivang Agarwal <itshivang@gmail.com>
2026-09-06 13:57:47 +0530

    Time: 1547 ms (40.83%), Space: 12.5 MB (92.46%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -4,18 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dp = []
         n = len(s)
-        for i in range(n+1):
-            arr = [0]*(n+1)
-            dp.append(arr)
+        prev = [0]*(n+1)
         for i in range(1,n+1):
+            curr = [0]*(n+1)
             for j in range(1,n+1):
                 if s[i-1] == s[n-j]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    curr[j] = 1 + prev[j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return dp[-1][-1]
+                    curr[j] = max(prev[j], curr[j-1])
+            prev = curr
+        return prev[-1]
 
 
 
