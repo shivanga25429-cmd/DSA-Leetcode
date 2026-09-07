1b1f3627315b702fb35e6b96e700e87b6a356491
Shivang Agarwal <itshivang@gmail.com>
2026-09-07 22:40:40 +0530

    Time: 7 ms (89.85%), Space: 12.4 MB (78.71%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -5,48 +5,44 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        # dp = []
-        # for i in range(len(s)):
-        #     arr = [None]*len(p)
-        #     dp.append(arr)
-        # def backtrack(i,j):
-        #     if i ==j == -1:
-        #         return True
-        #     elif i<0 or j<0:
-        #         return False
-        #     if dp[i][j]!= None:
-        #         return dp[i][j]
-        #     if s[i] == p[j] or p[j] == "?":
-        #         dp[i][j] =  backtrack(i-1,j-1)
-        #     elif p[j] == "*":
-        #         move = backtrack(i-1,j-1)
-        #         nmove = backtrack(i-1,j)
-        #         dmove = backtrack(i,j-1)
-        #         dp[i][j] =  move or nmove or dmove
-        #     else:
-        #         dp[i][j] =  False
-        #     return dp[i][j]
-        # return backtrack(len(s)-1,len(p)-1)
+        i = 0
+        j = 0
 
+        # Position of the last '*'
+        star = -1
 
-        dp = []
-        for i in range(len(s)+1):
-            arr = [None]*(len(p)+1)
-            arr[0] = False
-            dp.append(arr)
-        dp[0][0] = True
-        for j in range(1,len(p)+1):
-            dp[0][j] = dp[0][j-1] and p[j-1] == "*"
-        
+        # Position in s where '*' started matching
+        match = 0
 
-        for i in range(1,len(s)+1):
-            for j in range(1,len(p)+1):
-                if s[i-1] == p[j-1] or p[j-1] == "?":
-                    dp[i][j] =  dp[i-1][j-1]
-                elif p[j-1] == "*":
-                    dp[i][j] = dp[i-1][j] or dp[i][j-1]
-                else:
-                    dp[i][j] =  False
-        return dp[-1][-1]
+        while i < len(s):
+
+            # Normal character or '?'
+            if j < len(p) and (p[j] == s[i] or p[j] == '?'):
+                i += 1
+                j += 1
+
+            # Found '*'
+            elif j < len(p) and p[j] == '*':
+                star = j
+                match = i
+                j += 1
+
+            # Mismatch, but we have a previous '*'
+            elif star != -1:
+                j = star + 1
+                match += 1
+                i = match
+
+            # Mismatch and no '*'
+            else:
+                return False
+
+        # Remaining pattern must contain only '*'
+        while j < len(p):
+            if p[j] != '*':
+                return False
+            j += 1
+
+        return True
 
         
