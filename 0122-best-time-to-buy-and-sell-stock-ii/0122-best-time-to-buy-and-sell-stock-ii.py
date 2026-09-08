9cb0ac677217c7ae0fe4e8a7e9f9334f30a3bc82
Shivang Agarwal <itshivang@gmail.com>
2026-09-08 22:09:49 +0530

    Time: 0 ms (100%), Space: 13.7 MB (67.21%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # dp = []
+        # for i in range(len(prices)):
+        #     dp.append([-1,-1])
+        # def backtrack(i,buy):
+        #     if i == len(prices)-1:
+        #         if buy:
+        #             dp[i][buy] = 0
+        #             return dp[i][buy]
+        #         dp[i][buy] = prices[i]
+        #         return dp[i][buy]
+        #     if dp[i][buy]!=-1:
+        #         return dp[i][buy]
+        #     if buy:
+        #         dp[i][buy]=max(-prices[i]+backtrack(i+1,0), backtrack(i+1,1))    
+        #     else:
+        #         dp[i][buy]=max(prices[i]+backtrack(i+1,1), backtrack(i+1,0))
+        #     return dp[i][buy]
+                
+        # return backtrack(0,1)
+
+        # # dp[-1][1] = 0
+        # # dp[-1][0] = prices[-1]
+        # # for i in range(len(prices)-1,-1):
+        # #     for j in range(2):
+        # #         if buy:
+        # #             dp[i][j]=max(-prices[i]+dp[i+1][0], dp[i+1][1])    
+        # #         else:
+        # #             dp[i][j]=max(prices[i]+dp[i+1][1], dp[i+1][0])
+        # # return max(dp[0][0],dp[0][1],0)
+        
+        profit = 0
+        for i in range(1,len(prices)):
+            if prices[i-1]<prices[i]:
+                profit+=prices[i]-prices[i-1]
+        return profit
+
+
