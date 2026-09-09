afe77580d576568b2d67e7ac4a1e3e96549815bf
Shivang Agarwal <itshivang@gmail.com>
2026-09-09 13:15:08 +0530

    Time: 0 ms (100%), Space: 12.4 MB (30%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -4,11 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        i = 1
         temp = 999
         ans = 0
         while n>temp:
             ans += (n-temp)
-            i+=1
             temp = temp*1000+999
         return ans
