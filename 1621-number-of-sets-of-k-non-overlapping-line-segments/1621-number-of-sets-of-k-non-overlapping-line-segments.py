d9edaca673db3144d23492bfd15ebb593520e6bf
Shivang Agarwal <itshivang@gmail.com>
2026-09-16 10:18:02 +0530

    Time: 4 ms (71.43%), Space: 12.4 MB (100%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -5,23 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        prev = []
-        for j in range(2):
-            arr = [1]+[0]*(k)
-            prev.append(arr)
-        for ind in range(1,n+1):
-            curr = []
-            for j in range(2):
-                arr = [1]+[0]*(k)
-                curr.append(arr)
-            for allow in range(2):
-                for l in range(1,k+1):
-                    if allow:
-                        curr[allow][l] = max(curr[allow][l], curr[0][l-1] + prev[1][l])
-                    else:
-                        curr[allow][l] = max(curr[allow][l], prev[1][l]  + prev[0][l])
-            prev = curr
-        return (prev[0][-1])%(10**9+7)
+        c= 1
+        for i in range(1,n+k):
+            c = c*i
+        den1 = 1
+        for j in range(1,2*k+1):
+            den1 = den1*j
+        den2 = 1
+        for l in range(1,n-k):
+            den2 = den2*l
+        return (c//(den1*den2))%(10**9+7)
 
             
         
