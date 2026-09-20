2a66a76bfad983a8487cdc2b1b95a9d986c15d2e
Shivang Agarwal <itshivang@gmail.com>
2026-09-20 18:34:28 +0530

    Time: 8 ms (77.12%), Space: 12.5 MB (0.74%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -1,17 +1,6 @@
 class Solution(object):
     def reverseDegree(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        dic = {
-                'a': 26, 'b': 25, 'c': 24, 'd': 23, 'e': 22, 'f': 21,
-                'g': 20, 'h': 19, 'i': 18, 'j': 17, 'k': 16, 'l': 15,
-                'm': 14, 'n': 13, 'o': 12, 'p': 11, 'q': 10, 'r': 9,
-                's': 8, 't': 7, 'u': 6, 'v': 5, 'w': 4, 'x': 3, 'y': 2,
-                'z': 1
-        }
-        count = 0
+        p = 0
         for i in range(len(s)):
-            count += dic[s[i]]*(i+1)
-        return count
+            p += (123 - ord(s[i])) * (i+1)
+        return p
